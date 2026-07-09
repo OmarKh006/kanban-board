f1545b44656769449478186b399205538cbe093e
OmarKh006 <khairyomar006@gmail.com>
2026-07-09 18:24:03 +0300

    dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import Button from "./components/Button";
+import DropdownPrimitive from "./components/DropdownPrimitive";
 import TextField from "./components/TextField";
 
 function App() {
   return (
-    <h1 className="text-3xl text-main-purple text-heading-xl">
+    <div className="text-main-purple text-heading-xl text-3xl">
       <Button variant="destructive" size="lg">
         Hello World!
       </Button>
       <TextField placeholder={"test"} />
-    </h1>
+      <DropdownPrimitive
+        items={{
+          delete: {
+            label: "Delete Board",
+            onClick: () => console.log("delete"),
+          },
+          edit: {
+            label: "Edit Board",
+            onClick: () => console.log("edit"),
+          },
+        }}
+        triggerComponent={() => <Button variant="secondary">Actions</Button>}
+      />
+    </div>
   );
 }
 
