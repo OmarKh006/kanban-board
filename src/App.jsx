0e7af84c15a7be6aafaf34a0c651d305204ac5da
OmarKh006 <khairyomar006@gmail.com>
2026-07-09 20:50:37 +0300

    header layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import Button from "@components/Button";
-import DialogPrimitive from "@components/DialogPrimitive";
-import DropdownPrimitive from "@components/DropdownPrimitive";
-import TextField from "@components/TextField";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="text-main-purple text-heading-xl text-3xl">
-      <Button variant="destructive" size="lg">
-        Hello World!
-      </Button>
-      <TextField placeholder={"test"} />
-      <DropdownPrimitive
-        items={{
-          delete: {
-            label: "Delete Board",
-            onClick: () => console.log("delete"),
-          },
-          edit: {
-            label: "Edit Board",
-            onClick: () => console.log("edit"),
-          },
-        }}
-        triggerComponent={() => <Button variant="secondary">Actions</Button>}
-      />
-      <DialogPrimitive isOpen={false}>Hello</DialogPrimitive>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import DropdownPrimitive from "./DropdownPrimitive";
+import iconVerticalEllipsis from "@assets/icon-vertical-ellipsis.svg";
+import DialogPrimitive from "./DialogPrimitive";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const onEditBoard = () => setOpen(true);
+
+  const onDeleteBoard = () => {
+    if (window.confirm("Are you sure you want to delete this board ??")) {
+      console.log("delete");
+    }
+  };
+
+  return (
+    <header className="flex h-24.25 shrink-0 items-center">
+      <div className="border-lines-light flex w-75 items-center gap-4 self-stretch border-r border-b pl-8 text-[32px] font-bold">
+        Kanban
+      </div>
+      <div className="border-lines-light flex flex-1 items-center justify-between self-stretch border-b pr-6 pl-6">
+        <h2 className="text-heading-xl">Platform Launch</h2>
+        <DropdownPrimitive
+          items={{
+            edit: { label: "Edit Board", onClick: onEditBoard },
+            delete: {
+              label: "Delete Board",
+              onClick: onDeleteBoard,
+            },
+          }}
+          triggerComponent={() => (
+            <button className="text-main-purple flex items-center gap-2 text-[14px] font-bold">
+              <img src={iconVerticalEllipsis} alt="icon vertical ellipsis" />
+            </button>
+          )}
+        />
+        <DialogPrimitive isOpen={open} setOpen={setOpen} title="Edit Board">
+          Hello
+        </DialogPrimitive>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
