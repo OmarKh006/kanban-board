b2f1ddccc2269df770480230908a688e45106099
OmarKh006 <khairyomar006@gmail.com>
2026-07-09 18:00:16 +0300

    button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Button from "./components/Button/Button";
+
 function App() {
-  return <h1 className="text-3xl text-main-purple text-heading-xl">Omar</h1>;
+  return (
+    <h1 className="text-3xl text-main-purple text-heading-xl">
+      <Button variant="destructive" size="lg">
+        Hello World!
+      </Button>
+    </h1>
+  );
 }
 
 export default App;
