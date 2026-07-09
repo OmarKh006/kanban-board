db158d991220255426fddcb72f632c9cc18a51dc
OmarKh006 <khairyomar006@gmail.com>
2026-07-09 21:21:30 +0300

    side menu layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
-    <div className="text-main-purple text-heading-xl text-3xl">
+    <div className="flex h-screen flex-col text-3xl">
       <Header />
+      <div className="flex flex-1">
+        <SideMenu />
+      </div>
     </div>
   );
 }
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import { useState } from "react";
+import DialogPrimitive from "./DialogPrimitive";
+import iconBoard from "@assets/icon-board.svg";
+
+const DATA = [
+  { title: "Home", id: "1" },
+  { title: "About", id: "2" },
+  { title: "Contact", id: "3" },
+];
+
+const SideMenu = () => {
+  const [selectedBoardIndex, setSelectedBoardIndex] = useState(0);
+  const [open, setOpen] = useState(false);
+  return (
+    <aside className="side-menu border-lines-light -mt-px w-75 border-r bg-white">
+      <p className="text-heading-s px-8 py-4">All Boards ({DATA.length})</p>
+      <ul>
+        {DATA.map((item, index) => {
+          return (
+            <li key={item.id}>
+              <button
+                data-isactive={selectedBoardIndex === index}
+                className={clsx(
+                  "text-heading-m text-medium-grey data-[isactive=false]:hover:bg-main-purple/10 data-[isactive=false]:hover:text-main-purple flex w-11/12 items-center gap-4 rounded-e-full px-8 py-4 transition",
+                  {
+                    "bg-main-purple hover:bg-main-purple text-white!":
+                      selectedBoardIndex === index,
+                  },
+                )}
+                onClick={() => setSelectedBoardIndex(index)}
+              >
+                <img src={iconBoard} alt="" /> {item.title}
+              </button>
+            </li>
+          );
+        })}
+        <li className="px-8 py-4">
+          <DialogPrimitive
+            isOpen={open}
+            setOpen={setOpen}
+            title="Create New Board"
+            triggerComponent={
+              <button className="text-heading-m text-main-purple flex w-full items-center gap-4">
+                <img src={iconBoard} alt="" /> + Create New Board
+              </button>
+            }
+          >
+            Hello
+          </DialogPrimitive>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default SideMenu;
