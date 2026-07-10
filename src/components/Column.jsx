c230b2ef52cb6119131dc99a6569a92d68b7afd2
OmarKh006 <khairyomar006@gmail.com>
2026-07-10 15:11:13 +0300

    add new task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,13 +1,45 @@
+import { useContext } from "react";
 import Card from "./Card";
+import { DataContext } from "@/DataContext";
 
 /**
  * @param {Object} props
  * @param {Array} props.tasks - An array of task objects, each containing an id and title.
+ * @param {number} props.id
  * @param {string} props.title - The title of the column.
  * @returns {JSX.Element} A React component that renders a column with a list of tasks and a button to add a new task.
  */
 
-const Column = ({ tasks = [], title }) => {
+const Column = ({ id, tasks = [], title }) => {
+  const { selectedBoardIndex, data, setData } = useContext(DataContext);
+
+  const createNewTaskObject = () => ({ id: Date.now(), title: "New Task" });
+
+  const createNewColumnArray = (dataArray, boardIndex, newTask) => {
+    return dataArray[boardIndex].columns.map((column) => {
+      if (column.id === id) {
+        return {
+          ...column,
+          tasks: [...column.tasks, newTask],
+        };
+      }
+      return column;
+    });
+  };
+
+  const addNewTaskHandler = () => {
+    const newTask = createNewTaskObject();
+    const newColumns = createNewColumnArray(data, selectedBoardIndex, newTask);
+    setData((prev) => {
+      const newData = [...prev];
+      newData[selectedBoardIndex] = {
+        ...newData[selectedBoardIndex],
+        columns: newColumns,
+      };
+      return newData;
+    });
+  };
+
   return (
     <div className="bg-lines-light flex w-72 shrink-0 flex-col self-start rounded-lg px-2 shadow">
       <h2 className="group/column bg-lines-light text-heading-s relative top-0 rounded px-2 py-4">
@@ -18,7 +50,10 @@ const Column = ({ tasks = [], title }) => {
           <Card key={task.id} title={task.title} />
         ))}
       </div>
-      <button className="border-light-grey bg-lines-light text-heading-m text-medium-grey -mx-2 mt-auto border-t px-2 py-4">
+      <button
+        className="border-light-grey bg-lines-light text-heading-m text-medium-grey -mx-2 mt-auto border-t px-2 py-4"
+        onClick={addNewTaskHandler}
+      >
         + Add New Task
       </button>
     </div>
--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -9,7 +9,12 @@ const Workspace = () => {
     <div className="bg-light-grey flex h-[calc(100vh-97px)] flex-1 gap-6 overflow-auto p-6">
       {columns?.length &&
         columns.map((column) => (
-          <Column key={column.id} tasks={column.tasks} title={column.title} />
+          <Column
+            key={column.id}
+            id={column.id}
+            tasks={column.tasks}
+            title={column.title}
+          />
         ))}
       <button className="bg-lines-light text-heading-l text-medium-grey w-72 shrink-0 self-start rounded-md p-3">
         + New Column
