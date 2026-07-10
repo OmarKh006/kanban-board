88cdf40a9cd97b75c836b78cec2fc6a2c53e75f0
OmarKh006 <khairyomar006@gmail.com>
2026-07-10 03:09:57 +0300

    adding & removing columns from form

--- src/components/NewBoardForm.jsx
+++ src/components/NewBoardForm.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Button from "./Button";
 import TextField from "./TextField";
 import iconCross from "@assets/icon-cross.svg";
 
 const NewBoardForm = () => {
+  const [columnsArray, setColumnsArray] = useState(() => [{ id: Date.now() }]);
+
+  const removeColumnHandler = (index) => {
+    setColumnsArray((prev) => prev.filter((_, idx) => idx !== index));
+  };
+
+  const addNewColumnHandler = () => {
+    setColumnsArray((prev) => [...prev, { id: Date.now() }]);
+  };
+
   return (
     <form>
       <div>
@@ -11,13 +22,25 @@ const NewBoardForm = () => {
       </div>
       <div className="flex flex-col gap-2">
         <h3 className="text-body-m text-medium-grey pt-6">Columns</h3>
-        <div className="flex items-center gap-4">
-          <TextField placeholder="e.g. Web Design" name="boardName" required />
-          <button onClick={false}>
-            <img src={iconCross} alt="icon cross" />
-          </button>
-        </div>
-        <Button type="button" variant="secondary" size="sm" onClick={false}>
+        {columnsArray.map((obj, index) => (
+          <div key={obj.id} className="flex items-center gap-4">
+            <TextField
+              placeholder="e.g. Web Design"
+              name={obj.id}
+              defaultValue={obj.title}
+              required
+            />
+            <button onClick={() => removeColumnHandler(index)}>
+              <img src={iconCross} alt="icon cross" />
+            </button>
+          </div>
+        ))}
+        <Button
+          type="button"
+          variant="secondary"
+          size="sm"
+          onClick={addNewColumnHandler}
+        >
           + Add New Column
         </Button>
       </div>
